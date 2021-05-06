c067b43f223a3e38d4add7f0ce10026f9f61ea1f
Bese Gabor <gaborbese11@gmail.com>
2021-05-06 19:57:29 +0200

    Defining code behind

--- BeadandoBeseGabor/Invoice.cs
+++ BeadandoBeseGabor/Invoice.cs
@@ -3,9 +3,13 @@
 	
 	class Invoice
 	{
-		private int _id { get; set; }
-		private string _owner { get; set; }
-		private int _amount { get; set; }
+		private int _id;
+		private string _owner;
+		private int _amount;
+
+		public int id { get; set; }
+		public int owner { get; set; }
+		public int amount { get; set; }
 
 		public Invoice(int id, string owner, int amount)
 		{
--- BeadandoBeseGabor/MainWindow.xaml.cs
+++ BeadandoBeseGabor/MainWindow.xaml.cs
@@ -20,9 +20,80 @@ namespace BeadandoBeseGabor
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Invoice invoiceOne = new Invoice(1, "Balázs", 5000);
+		Invoice invoiceTwo = new Invoice(2, "Patrik", 5000);
+
 		public MainWindow()
 		{
-			InitializeComponent();
+			InitializeComponent();			
+		}
+
+		private void Deposite(Invoice invoice, int amount)
+		{
+			try
+			{
+				if (amount > 0)
+				{
+					invoice.amount += amount;
+					RefreshUI();
+				}
+				else
+				{
+					throw new ArgumentException();
+				}
+			}
+			catch (ArgumentException)
+			{
+
+				MessageBox.Show("Csak pozitív egész szám adaható meg a tranzakció értékeként", "Hiba történt!", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void Transfer(Invoice sender, Invoice destination, int amount)
+		{
+			try
+			{
+				if (sender.amount > amount)
+				{
+					sender.amount -= amount;
+					destination.amount += amount;
+					RefreshUI();
+				}
+				else
+				{
+					throw new ArgumentException();
+				}
+			}
+			catch (ArgumentException)
+			{
+				MessageBox.Show("Az utaláshoz rendelkezni kell elegendő összeggel illetve csak pozitív egész szám adaható meg a tranzakció értékeként", "Hiba történt!", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void Withdraw(Invoice invoice, int amount)
+		{
+			try
+			{
+				if (amount > 0 && invoice.amount >= amount)
+				{
+					invoice.amount -= amount;
+					RefreshUI();
+				}
+				else
+				{
+					throw new ArgumentException();
+				}
+			}
+			catch (ArgumentException)
+			{
+				MessageBox.Show("Csak pozitív egész szám adaható meg a tranzakció értékeként", "Hiba történt!", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void RefreshUI()
+		{
+			FirstMoneyTextBox.Text = invoiceOne.amount.ToString();
+			SecondMoneyTextBox.Text = invoiceTwo.amount.ToString();
 		}
 	}
 }
