6166b7ef1f821f347b72415fa21d1cd423df918c
Bese Gabor <gaborbese11@gmail.com>
2021-05-06 19:29:22 +0200

    Creation of the UI and Invoice Class

---
+++ BeadandoBeseGabor/Invoice.cs
@@ -0,0 +1,17 @@
+﻿namespace BeadandoBeseGabor
+{
+	
+	class Invoice
+	{
+		private int _id { get; set; }
+		private string _owner { get; set; }
+		private int _amount { get; set; }
+
+		public Invoice(int id, string owner, int amount)
+		{
+			_id = id;
+			_owner = owner;
+			_amount = amount;
+		}
+	}
+}
